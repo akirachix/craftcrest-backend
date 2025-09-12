129f4ad6f31d2ca2f434f05cfc5cc210f9a3de0c
studen <jecintawanjirumuhia@gmail.com>
2025-09-12 15:11:31 +0300

    change  the naming of the models' foreign key

--- products/models.py
+++ products/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from users.models import User
 class Inventory(models.Model):
+    CATEGORY_CHOICES = [
+    ('pottery', 'Pottery'),
+    ('tailoring', 'Tailoring'),
+    ('basketry', 'Basketry'),
+    ('weaving', 'Weaving'),
+    ('crocheting', 'Crocheting'),
+    ('ceramics', 'Ceramics'),
+   
+]
     artisan_id = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'user_type': 'artisan'})
     product_name = models.CharField(max_length=100)
     description = models.TextField()
-    category = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock_quantity = models.IntegerField()
     image_url = models.URLField()
