e013b62eb8e48e4b9526f7b5c27c0ee11cdc5e89
khaenzelidorothy <khaenzelidorothy@gmail.com>
2025-09-15 17:34:25 +0300

    added swagger documentation

--- craftcrest/settings.py
+++ craftcrest/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'products',
     'orders',
     'cart',
+    'drf_spectacular',
+    'drf_spectacular_sidecar',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'CraftCrest API',
+    'DESCRIPTION': 'CraftCrest is a online marketplace that enables skilled artisans to showcase their craftsmanship.',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    'SWAGGER_UI_DIST': 'SIDECAR',
+    'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
+    'REDOC_DIST': 'SIDECAR',
+}
