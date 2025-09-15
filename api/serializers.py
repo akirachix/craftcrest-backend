ead2d3c07866a545f69e2cfb4e8ff64d81b80df0
Daniella Syombua <syombuadaniella@gmail.com>
2025-09-15 15:20:29 +0300

    Created the users api and tested it's endpoints

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,3 @@
-
 from django.core.validators import validate_email, URLValidator
 from django.core.exceptions import ValidationError as DjangoValidationError
 from django.contrib.auth.password_validation import validate_password
