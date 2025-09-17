f37bd1909f4707bc1575334e5900285fc8d3ec8d
Daniella Syombua <syombuadaniella@gmail.com>
2025-09-17 14:38:32 +0300

    Added a line of code to signals.py

--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from users.models import User, Profile
+from users.models import ArtisanProfile
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
