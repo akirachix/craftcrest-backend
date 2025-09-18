82b41fa8b2b8c9eb44ea9a7e50ea6b214e9765cc
khaenzelidorothy <khaenzelidorothy@gmail.com>
2025-09-18 09:11:25 +0300

    created models

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,1 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from api.views import (
-    OrderViewSet, RatingViewSet,
-    OrderStatusViewSet, CustomDesignRequestViewSet
-)
 
-router = DefaultRouter()
-router.register(r'orders', OrderViewSet, basename='order')
-router.register(r'ratings', RatingViewSet, basename='rating')
-router.register(r'trackings', OrderStatusViewSet, basename='orderstatus')
-router.register(r'custom-requests', CustomDesignRequestViewSet, basename='customdesignrequest')
-
-urlpatterns = [
-    path('api/', include(router.urls)),
-]
