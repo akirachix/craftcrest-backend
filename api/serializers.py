17ca7b9817f7a9461143cc6cbce9daaa96dac082
studen <jecintawanjirumuhia@gmail.com>
2025-09-18 08:10:37 +0300

    created views and serialiser for  cart and products

--- api/serializers.py
+++ api/serializers.py
@@ -4,17 +4,32 @@ from products.models import Inventory
 
 
 from rest_framework import serializers
-from cart.models import ShoppingCart
+from cart.models import ShoppingCart , Item
 
 class ShoppingCartSerializer(serializers.ModelSerializer):
+    item = serializers.PrimaryKeyRelatedField(many=True, queryset=Item.objects.all())
     class Meta:
         model = ShoppingCart
         fields = '__all__'
-       
-
-
+    def update(self, instance, validated_data):
+        items = validated_data.pop('item', None)
+        if items is not None:
+            instance.item.set(items)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+    
+class ItemSerializer (serializers.ModelSerializer):
+    class Meta:
+        model= Item
+        fields = '__all__'       
 
 class InventorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Inventory
         fields = '__all__'
+
+
+
+    
--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ShoppingCartViewSet,InventoryViewSet
+from .views import ShoppingCartViewSet,InventoryViewSet,ItemViewSet
 
 router = DefaultRouter()
 router.register(r'carts', ShoppingCartViewSet, basename='cart')
 router.register(r'inventory', InventoryViewSet, basename='inventory')
+router.register(r'items', ItemViewSet, basename='item')
+
 
 urlpatterns = [
     path('api/', include(router.urls)),
